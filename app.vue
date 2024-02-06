6aee20197dabd84ee2ba2a69377d148a9306d521
Eugene <evgenij.kulikov@gmail.com>
2024-02-06 18:24:22 +0100

    Adds custom colors props

--- app.vue
+++ app.vue
@@ -1,7 +1,10 @@
 <template>
-  <div></div>
+  <p class="block max-w-25 h-25 bg-primary-dark text-primary-light">
+    some text
+  </p>
 </template>
 
 <style>
 @import url("~/assets/styles/reset.css");
+@import url("~/assets/styles/variables.css");
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: [],
   theme: {
+    colors: {
+      "primary-light": "hsl(var(--color-primary-light) / <alpha-value>)",
+      "primary-dark": "hsl(var(--color-primary-dark) / <alpha-value>)",
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
